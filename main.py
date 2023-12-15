5b0d7e3e36c529b7dfe787d9cce41c8969130231
ALBERTMICHAEL7 <6MICHA94@solent.ac.uk>
2023-12-15 18:28:15 +0000

    training of model com726

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from vgg16classifier import logger
 from vgg16classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from vgg16classifier.pipeline.stage_03_training import vgg16_ModelTrainingPipeline
 STAGE_NAME = "Prepare base model vgg16"
 try: 
    logger.info(f"*******************")
@@ -8,6 +9,16 @@ try:
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+STAGE_NAME = "running main file#Training"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = vgg16_ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
